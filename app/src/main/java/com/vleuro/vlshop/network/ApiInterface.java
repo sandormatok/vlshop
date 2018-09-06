8089c1b925e4ff079b9c86baa6a3d97de5901369
san <matok.sandor@suriel.eu>
2018-09-06 10:40:12 +0200

    + working drawer!!!
    ____________
    + butterknife
    + materialdrawer

---
+++ app/src/main/java/com/vleuro/vlshop/network/ApiInterface.java
@@ -0,0 +1,17 @@
+package com.vleuro.vlshop.network;
+
+import java.util.List;
+
+import com.vleuro.vlshop.model.Message;
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+/**
+ * Created by Ravi Tamada on 21/02/17.
+ * www.androidhive.info
+ */
+
+public interface ApiInterface {
+    @GET("inbox.json")
+    Call<List<Message>> getInbox();
+}
